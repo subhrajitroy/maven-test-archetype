1640c10356b0ad287d3fc18f831c9a2526e5f48c
Subhrajit Roy <sroy@thoughtworks.com>
2013-01-17 19:05:03 +0530

    Added scipt to do release

--- release.sh
+++ release.sh
@@ -4,3 +4,9 @@ ARCHETYPE_METADATA_FILE=src/main/resources/META-INF/maven/archetype-metadata.xml
 
 
 sed -i '' -e "s/[.0-9]*-SNAPSHOT/${1}/g" ${ARCHETYPE_METADATA_FILE}
+
+git add ${ARCHETYPE_METADATA_FILE}
+
+git commit -m"Release|Changed motech version to ${ARCHETYPE_METADATA_FILE}"
+
+mvn release:perform
